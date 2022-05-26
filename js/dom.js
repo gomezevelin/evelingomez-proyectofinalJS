20e124af99de3a774963c189d9b33722eaa067fe
Evelin Gomez <gomez.evelin@hotmail.com>
2022-05-26 18:54:00 -0300

    Agregado de titulo y subtitulo por el dom

---
+++ js/dom.js
@@ -0,0 +1,17 @@
+    
+    document.body.style.background = '#A3E4D7 '
+    
+    const tituloPrincipal = document.createElement("h1");
+    tituloPrincipal.innerHTML="¿Que comemos hoy?";
+
+    tituloPrincipal.setAttribute("style","color:green;text-align:center; font-size: 160px");
+    tituloPrincipal.classList.add("miTitulo");
+    tituloPrincipal.classList.add("miTitulo2");
+
+    document.body.appendChild(tituloPrincipal);
+
+
+    const subtitulo = document.createElement("p");
+    subtitulo.innerText="Sabemos que esta pregunta nos cansa a todos. Queremos acercarte sugerencias, novedades y hasta la posibilidad de que elijas la receta según lo que tengas en la heladera. Creemos que la cena es una excusa para el encuentro y el disfrute, no queremos que nada te la arruine.";
+    subtitulo.setAttribute("style", "font-family:Segoe UI', Tahoma, Geneva, Verdana, sans-serif; text-align:center")
+    document.body.appendChild(subtitulo);
