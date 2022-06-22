4dedb405a00c235f88c0eaa5b92dcb8eaa8cd6b8
Evelin Gomez <gomez.evelin@hotmail.com>
2022-06-22 19:18:17 -0300

    se suma favoritos.js, se desarrolla la funcion

--- js/favoritos.js
+++ js/favoritos.js
@@ -1,18 +1,35 @@
 function rellenarRecetasFavoritas() {
-    let arr = JSON.parse(localStorage.getItem("favoritos"))
+    let arr = JSON.parse(localStorage.getItem("favoritos")) || [];
     let tBody = document.getElementById("tBodyFav");
     for (let receta of arr) {
       let trTbody = document.createElement("tr");
       trTbody.innerHTML = `
-        <td>${receta.titulo}</td>
-        <td>${receta.ingredientes}</td>
-        <td>${receta.tiempo}</td>
-        <td>>button${receta.preparacion}</button></td>
-        <td><button class="btn btn-danger eliminarProducto">Eliminar</button></td>`;
+          <td id="titulo">${receta.titulo}</td>
+          <td>${receta.ingredientes}</td>
+          <td>${receta.tiempo}</td>
+          <td><button class="btn btn-success verMas">Ver más</button></td>
+          <td><button class="btn btn-danger eliminarProducto">Eliminar</button></td>`;
       tBody.appendChild(trTbody);
     }
   }
-
-  rellenarRecetasFavoritas()
-
+  
+  rellenarRecetasFavoritas();
+  
+  
+  document.querySelectorAll(".verMas").forEach(boton => {
+    boton.addEventListener("click", verMas)
+  })
+  
+  function verMas(e){
+  
+    let arr = JSON.parse(localStorage.getItem("favoritos")) || [];
+    let titulo = e.target.parentNode.parentNode.children[0].innerText;
+  
+    let encontrado = arr.find(elemento => {
+      return elemento.titulo == titulo;
+    })
+  
+    e.target.innerText = encontrado.preparacion;
+    e.target.className = "";
+  }
   
